aac88d57a9ef180db6e4ecbd092d862ee3a38611
rhaix23 <patrick.lloyd.santos@gmail.com>
2022-09-23 04:49:53 +0800

    Updated Registration / Login Modal

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,20 +1,75 @@
-import { Modal } from '@mantine/core';
-import { toggleSignInModal } from '../features/users/userSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import {
+  Anchor,
+  Button,
+  Modal,
+  PasswordInput,
+  Text,
+  TextInput,
+} from "@mantine/core";
+import { toggleSignInModal } from "../features/users/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 
 const Registration = () => {
   const dispatch = useDispatch();
   const { isSigninIn } = useSelector((store) => store.users);
+  const [isNewUser, setIsNewUser] = useState(false);
 
   return (
     <>
       <Modal
         opened={isSigninIn}
-        onClose={() => dispatch(toggleSignInModal())}
-        title="Sign In"
+        onClose={() => {
+          setIsNewUser(false);
+          dispatch(toggleSignInModal());
+        }}
+        centered
+        title={isNewUser ? "Sign Up" : "Sign In"}
       >
         {/* Modal content HERE 🙋‍♂️ */}
-        <h3>Registration and Login Form </h3>
+        <form>
+          <TextInput
+            placeholder="Enter your email address"
+            label="Email Address"
+            radius="sm"
+            withAsterisk
+            mb={5}
+          />
+          <PasswordInput
+            placeholder="Enter your password"
+            label="Password"
+            mb={5}
+            withAsterisk
+          />
+          {isNewUser ? (
+            <PasswordInput
+              placeholder="Confirm your password"
+              label="Confirm Password"
+              mb={5}
+              withAsterisk
+            />
+          ) : (
+            <Anchor>
+              <Text align="right" sx={{ fontSize: 14 }}>
+                Forgot your password?
+              </Text>
+            </Anchor>
+          )}
+
+          <Button fullWidth mt={15}>
+            {isNewUser ? "Sign Up" : "Sign In"}
+          </Button>
+        </form>
+        <Text align="center" mt={15} sx={{ fontSize: 14 }}>
+          {isNewUser ? "Already have an account? " : "Don't have an account? "}
+          <Button
+            variant="subtle"
+            compact
+            onClick={() => setIsNewUser((prevState) => !prevState)}
+          >
+            {isNewUser ? "Sign In" : "Sign Up"}
+          </Button>
+        </Text>
       </Modal>
     </>
   );
