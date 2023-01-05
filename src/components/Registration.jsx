2e42aa5e6aa952cc35583fa7eda934a86967f8a6
rhaix23 <patrick.lloyd.santos@gmail.com>
2023-01-05 22:05:47 +0800

    Strong Password Validation Added

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,29 +1,30 @@
 import {
   Alert,
   Anchor,
+  Box,
   Button,
   Modal,
   PasswordInput,
   Text,
   TextInput,
-} from '@mantine/core';
+} from "@mantine/core";
 import {
   loginUser,
   registerUser,
   toggleForgotPasswordModal,
   toggleSignInModal,
-} from '../features/users/userSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
-import { IconAlertCircle } from '@tabler/icons';
-import { toast } from 'react-toastify';
-import { useEffect } from 'react';
+} from "../features/users/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useCallback, useMemo, useState } from "react";
+import { IconAlertCircle, IconCircleCheck } from "@tabler/icons";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
 const initialState = {
-  email: '',
-  username: '',
-  password: '',
-  confirmPassword: '',
+  email: "",
+  username: "",
+  password: "",
+  confirmPassword: "",
 };
 
 const Registration = () => {
@@ -31,16 +32,111 @@ const Registration = () => {
   const { isSigninIn, isLoading } = useSelector((store) => store.users);
   const [isNewUser, setIsNewUser] = useState(false);
   const [values, setValues] = useState(initialState);
+  const [errorMessages, setErrorMessages] = useState([]);
+  const [typedPasswordAtleastOnce, setTypedPasswordAtleastOnce] =
+    useState(false);
+
+  const handleErrorMessages = useCallback(() => {
+    const hasMinLength = new RegExp("^(?=.{8,})");
+    const hasOneUppercaseChar = new RegExp("^(?=.*[A-Z])");
+    const hasOneLowerCaseChar = new RegExp("^(?=.*[a-z])");
+    const hasOneDigit = new RegExp("^(?=.*[0-9])");
+    const hasOneSpecialChar = new RegExp("^(?=.*[!@#$%^&*])");
+
+    const message = []; /* {
+      text: string,
+      type: "success" | "error"
+    } */
+    const password = values.password;
+    const confirmPassword = values.confirmPassword;
+
+    if (!hasMinLength.test(password)) {
+      message.push({
+        text: "Password must contain at least 8 characters",
+        type: "error",
+      });
+    } else {
+      message.push({
+        text: "Password contains at least 8 characters",
+        type: "success",
+      });
+    }
+
+    if (!hasOneUppercaseChar.test(password)) {
+      message.push({
+        text: "Password must contain at least one uppercase character",
+        type: "error",
+      });
+    } else {
+      message.push({
+        text: "Password contains at least one uppercase character",
+        type: "success",
+      });
+    }
+
+    if (!hasOneLowerCaseChar.test(password)) {
+      message.push({
+        text: "Password must contain at least one lowercase character",
+        type: "error",
+      });
+    } else {
+      message.push({
+        text: "Password contains at least one lowercase character",
+        type: "success",
+      });
+    }
+
+    if (!hasOneDigit.test(password)) {
+      message.push({
+        text: "Password must contain at least one digit",
+        type: "error",
+      });
+    } else {
+      message.push({
+        text: "Password contains at least one digit",
+        type: "success",
+      });
+    }
+
+    if (!hasOneSpecialChar.test(password)) {
+      message.push({
+        text: "Password must contain at least one special character",
+        type: "error",
+      });
+    } else {
+      message.push({
+        text: "Password contains at least one special character",
+        type: "success",
+      });
+    }
+
+    if (password === confirmPassword && password.length !== 0) {
+      message.push({
+        text: "Passwords match",
+        type: "success",
+      });
+    } else {
+      message.push({
+        text: "Passwords do not match",
+        type: "error",
+      });
+    }
+
+    setErrorMessages(message);
+  }, [values.password, values.confirmPassword]);
 
   const handleChange = (e) => {
+    if (isNewUser && e.target.name === "password") {
+      setTypedPasswordAtleastOnce(true);
+    }
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { username, email, password, confirmPassword } = values;
-    if (!username || !password || (isNewUser && !confirmPassword && !email)) {
-      toast.warning('Please provide all credentials');
+    const { username, email, password } = values;
+    if (!username || !password || (isNewUser && errorMessages.length > 0)) {
+      toast.warning("Please provide all credentials");
       return;
     }
 
@@ -51,6 +147,12 @@ const Registration = () => {
 
     dispatch(registerUser({ username, email, password }));
   };
+
+  useEffect(() => {
+    if (isNewUser && typedPasswordAtleastOnce) {
+      handleErrorMessages();
+    }
+  }, [isNewUser, typedPasswordAtleastOnce, handleErrorMessages]);
 
   useEffect(() => {
     setValues(initialState);
@@ -65,7 +167,7 @@ const Registration = () => {
           dispatch(toggleSignInModal());
         }}
         centered
-        title={isNewUser ? 'Sign Up' : 'Sign In'}
+        title={isNewUser ? "Sign Up" : "Sign In"}
       >
         {/* Modal content HERE 🙋‍♂️ */}
         <form>
@@ -116,13 +218,25 @@ const Registration = () => {
                 value={values.confirmPassword}
                 onChange={handleChange}
               />
-
-              {values.confirmPassword &&
-                values.password !== values.confirmPassword && (
-                  <Alert icon={<IconAlertCircle size={16} />} color="red">
-                    Passwords do not match
-                  </Alert>
-                )}
+              {typedPasswordAtleastOnce &&
+                errorMessages.map((message) => (
+                  <Box
+                    sx={{ display: "flex", alignItems: "center", gap: "4px" }}
+                    key={message._id}
+                  >
+                    {message.type === "error" ? (
+                      <IconAlertCircle size={12} color="red" />
+                    ) : (
+                      <IconCircleCheck size={12} color="green" />
+                    )}
+                    <Text
+                      color={message.type === "error" ? "red" : "green"}
+                      size={12}
+                    >
+                      {message.text}
+                    </Text>
+                  </Box>
+                ))}
             </>
           ) : (
             <Anchor>
@@ -143,18 +257,18 @@ const Registration = () => {
             type="submit"
             onClick={handleSubmit}
           >
-            {isNewUser ? 'Sign Up' : 'Sign In'}
+            {isNewUser ? "Sign Up" : "Sign In"}
           </Button>
         </form>
 
         <Text align="center" mt={15} sx={{ fontSize: 14 }}>
-          {isNewUser ? 'Already have an account? ' : "Don't have an account? "}
+          {isNewUser ? "Already have an account? " : "Don't have an account? "}
           <Button
             variant="subtle"
             compact
             onClick={() => setIsNewUser((prevState) => !prevState)}
           >
-            {isNewUser ? 'Sign In' : 'Sign Up'}
+            {isNewUser ? "Sign In" : "Sign Up"}
           </Button>
         </Text>
       </Modal>
